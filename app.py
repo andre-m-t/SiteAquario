48bfdc66e0cfe45d190af943fcd471823c4ea5be
andre-m-t <andre_moreira.9@hotmail.com>
2024-06-01 13:46:49 -0300

    Finalização da funcionalidade de controle! (atenção no ajuste manual de temperatura para o sistema desligado)

--- app.py
+++ app.py
@@ -53,58 +53,112 @@ def projeto():
 
 @app.route('/ControleTemperatura', methods=['POST'])
 def comunicacao_com_sistema():
+
+    # Validando set Point
     try:
         set_point = float(request.form['set_point'])
         temperatura = float(request.form['tp_atual'])
     except (ValueError, KeyError) as e:
         return jsonify({"error": "Dados inválidos fornecidos."}), 400
+    
+    # Variavel global para controle
     global processo
     processo = True
+    # Print da temp e set point atual
     print("temp: ", temperatura, "\nSp: ", set_point)
 
+    # Processo de resfriar 
     if temperatura > set_point:
-        atualiza_processo("Resfriando")
+        atualiza_processo(2)
         resfriando = True
         while resfriando:
-            temperatura = buscar_temperatura_atual(20.0)
+            temperatura = buscar_temperatura_atual()
             print("Resfriando... Tp atual:", temperatura)
-            if not processo:
-                return jsonify({"response": "ok"}), 200  # Use break para sair do loop
-            if temperatura <= set_point:
+            # Ponto de parada do processo
+            if temperatura <= set_point or not processo:
                 resfriando = False
-                atualiza_processo("Parado")
+                atualiza_processo(1)
                 return jsonify({"response": "ok"}), 200  # Use break para sair do loop
-
+    # Processo de aquecer
     elif temperatura < set_point:
-        atualiza_processo("Esquentando")
+        atualiza_processo(3)
         esquentando = True
         while esquentando:
-            temperatura = buscar_temperatura_atual(20.0)
+            temperatura = buscar_temperatura_atual()
             print("Esquentando... Tp atual:", temperatura)
-            if not processo:
-                return jsonify({"response": "ok"}), 200  # Use break para sair do loop
-            if temperatura >= set_point:  # Corrigido de <= para >=
+            # Ponto de parada do processo
+            if temperatura >= set_point or not processo:  # Corrigido de <= para >=
                 esquentando = False
-                atualiza_processo("Parado")
+                atualiza_processo(1)
                 return jsonify({"response": "ok"}), 200  # Use break para sair do loop
 
     else:
-        atualiza_processo("Parado")
+        atualiza_processo(1)
 
     return jsonify({"response": "ok"}), 200
 
-def buscar_temperatura_atual(tp):
-    time.sleep(2)
-    tp += random.uniform(-10.5, 10.5)
-    return tp
+def buscar_temperatura_atual():
+    # Query de consulta para buscar ultima temperatura
+    query = "SELECT valor_temperatura from aquariogohan.Dados WHERE id = (SELECT max(id) from aquariogohan.Dados);"
+    # Conectar ao banco de dados
+    conexao = mysql.connector.connect(**db_config)
 
-def atualiza_processo(acao:str):
-    pass
+    # Criar um objeto cursor para interagir com o banco de dados
+    cursor = conexao.cursor()
+
+    # Exemplo de consulta SELECT
+    cursor.execute(query)
+
+    # Recebendo dados
+    temp = cursor.fetchall()[0][0]
+
+    # Pausa para ajuste no sistema
+    # time.sleep(2)
+
+    # ajuste manual para teste com projeto desligado
+    temp += random.uniform(-10.5, 10.5)
+
+    return temp
+
+def atualiza_processo(acao:int):
+
+    # Conectar ao banco de dados
+    conexao = mysql.connector.connect(**db_config)
+
+    # Criar um objeto cursor para interagir com o banco de dados
+    cursor = conexao.cursor()
+
+    # Definindo query de acordo com o processo
+    if acao == 1: 
+        query = "INSERT INTO aquariogohan.Processo(codigo_processo, descricao_processo) VALUES (1, 'PARADO');"
+        print("Parado")
+    elif acao == 2:
+        query = "INSERT INTO aquariogohan.Processo(codigo_processo, descricao_processo) VALUES (2, 'RESFRIANDO');"
+        print("Resfriando")
+    elif acao == 3:
+        query = "INSERT INTO aquariogohan.Processo(codigo_processo, descricao_processo) VALUES (3, 'ESQUENTANDO');"
+        print("Esquentando")
+
+    # Executando query de insercao
+    cursor.execute(query)
+
+    # Deletando ultimo estado para evitar sobrecarga no banco
+    # Select no ultimo id
+    query  = "SELECT MIN(id) FROM aquariogohan.Processo"
+    cursor.execute(query)
+    last_id = cursor.fetchall()[0][0]
+    # Executando query de remocao para o id selecionado
+    query = "DELETE FROM aquariogohan.Processo WHERE id = %s"
+    cursor.execute(query, (last_id,))
+
+    # Commitando manipulacao
+    conexao.commit()
+
 @app.route('/CancelaProcesso', methods=['POST'])
 def interromper_processo():
     global processo
     processo = False
-    atualiza_processo("Parado")
+    atualiza_processo(1)
     return jsonify({"response": "Processo cancelado"}), 200
 
 @app.route('/FiltroAtivo', methods=['POST'])
